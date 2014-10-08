00fef28292dcdf5ab12ede9d7ed6d90a7a90d970
Aldo Mendez <aldomendez86@gmail.com>
2014-10-07 21:15:43 -0500

    Mejora: Limpieza de codigo
    - Ahora cuando mande correos de las personas que ma faltan de capturar, me
      mandara unicamente las que tengo que capturar, por que anteriormente, me
      mandaba en la lista el usuario NULL, que obviamente, no tiene correo y
      g_jeteda era el otro personaje que se que no tiene correo. (asi que no
      tengo como hacerle llegar las notificaciones.

--- coffee/emailjs.coffee
+++ coffee/emailjs.coffee
@@ -18,7 +18,7 @@ getJSON 'http://jupiter.avagotech.net/apps/qa/maint-dev/toolbox/toolbox.php?acti
 	# Despues a los que no esten asignados, les tengo que enviar un correo a los Supervisores, anunciando los mantenimientos que estan pendientes de asignar
 	# Y enviar las notificaciones a los usuarios que tienen mantenimientos pendientes
 	_.map _.groupBy(res, 'MAINT_OPER'), (el, name)->
-		unasigned.push "hello #{name} my email is #{el[0].EMAIL}, supervisor #{el[0].SUPERVISOR}" if !el[0].EMAIL?
+		unasigned.push {'name':name,'email':el[0].EMAIL,'supervisor':el[0].SUPERVISOR} if !el[0].EMAIL? and name isnt 'null' and name isnt 'g_tejeda'
 		if name isnt 'null' and el[0].EMAIL?
 			# Esta parte se ejecuta cuando 'name' (el nombre de la persona que tiene asignado el mantenimiento),
 			# no es 'null', o sea que alguien lo tiene asignado.
@@ -29,16 +29,18 @@ getJSON 'http://jupiter.avagotech.net/apps/qa/maint-dev/toolbox/toolbox.php?acti
 				mismos que tienes que completar durante la semana:\n\n
 				<% _.each(maint, function(single) {%><%= single.MAINT_ID %>: <%= single.E_DESC %>\n <% });%>\n\n\n
 				Si tienes problemas para completar los mantenimientos dirigete con #{supervisor}\n
+				Para cerrar revisar y completar tus mantenimientos: http://jupiter.avagotech.net/apps/qa/maintenance/login.php
 				Si tu correo '<%=maint[0].EMAIL%>' no corresponde con la cuenta '<%= name %>' envia un correo a
 				aldo.mendez@avagotech.com para hacer la correccion")({maint:el, name:name})
-			console.log Message
+			# console.log Message
 			server.send {
 				from:'MaintBot <cyopticsmexico@gmail.com>'
 				to:"aldo.mendez@avagotech.com,#{email}"
 				subject:'Mantenimientos'
 				text:Message
 			}, (err, message)->
-				# console.log "Error: #{err}" or "Message: #{message}"
+				console.log "Error: #{err}" if err
+				console.log "Message: #{message}" if message
 		if name is 'null'
 			# Esta parte se ejecuta cuando name es 'null', o sea que no se le a asignado a nadie
 			_.map _.groupBy( el,'SUPERVISOR'), (el, name)->
@@ -49,24 +51,28 @@ getJSON 'http://jupiter.avagotech.net/apps/qa/maint-dev/toolbox/toolbox.php?acti
 					Tienes [<%= maint.length%>] mantenimientos pendientes de asignar, 
 					asignalos a la brevedad posible:\n\n
 					<% _.each(maint, function(single) {%><%= single.MAINT_ID %>: <%= single.E_DESC %>\n <% });%>\n\n\n
+					Asigna  tus mantenimientos en la siguiente direccion: http://jupiter.avagotech.net/apps/qa/maint/index.php
 					Si tu correo '<%=maint[0].SUP_EMAIL%>' no corresponde con la cuenta '<%= name %>' envia un correo a
 					aldo.mendez@avagotech.com para hacer la correccion")({maint:el, name:name})
-				console.log Message
+				# console.log Message
 				server.send {
 					from:'MaintBot <cyopticsmexico@gmail.com>'
 					to:"aldo.mendez@avagotech.com,#{email}"
 					subject:'Mantenimientos'
 					text:Message
 				}, (err, message)->
-					# console.log "Error: #{err}" or "Message: #{message}"
+					console.log "Error: #{err}" if err
+					console.log "Message: #{message}" if message
 	# console.log unasigned
+	if unasigned.length is 0 then unasigned.push 'Todos los usuarios estan capturados'
 	server.send {
 		from:'MaintBot <cyopticsmexico@gmail.com>'
 		to:"aldo.mendez@avagotech.com"
 		subject:'Mantenimientos: faltantes para capturar'
 		text:"#{JSON.stringify(unasigned)}"
 	}, (err, message)->
-		# console.log "Error: #{err}" or "Message: #{message}"
+		console.log "Error: #{err}" if err
+		console.log "Message: #{message}" if message
 
 
 # para enviar el correo solo tengo que ejecutar
--- js/emailjs.js
+++ js/emailjs.js
@@ -21,47 +21,73 @@
   getJSON('http://jupiter.avagotech.net/apps/qa/maint-dev/toolbox/toolbox.php?action=getAllPending', function(err, res) {
     _.map(_.groupBy(res, 'MAINT_OPER'), function(el, name) {
       var Message, email, supervisor;
-      if (el[0].EMAIL == null) {
-        unasigned.push("hello " + name + " my email is " + el[0].EMAIL + ", supervisor " + el[0].SUPERVISOR);
+      if ((el[0].EMAIL == null) && name !== 'null' && name !== 'g_tejeda') {
+        unasigned.push({
+          'name': name,
+          'email': el[0].EMAIL,
+          'supervisor': el[0].SUPERVISOR
+        });
       }
       if (name !== 'null' && (el[0].EMAIL != null)) {
         email = el[0].EMAIL;
         supervisor = el[0].SUPERVISOR;
-        Message = _.template("<%= name %>\n\n Tienes [<%= maint.length%>] mantenimientos asignados, mismos que tienes que completar durante la semana:\n\n <% _.each(maint, function(single) {%><%= single.MAINT_ID %>: <%= single.E_DESC %>\n <% });%>\n\n\n Si tienes problemas para completar los mantenimientos dirigete con " + supervisor + "\n Si tu correo '<%=maint[0].EMAIL%>' no corresponde con la cuenta '<%= name %>' envia un correo a aldo.mendez@avagotech.com para hacer la correccion")({
+        Message = _.template("<%= name %>\n\n Tienes [<%= maint.length%>] mantenimientos asignados, mismos que tienes que completar durante la semana:\n\n <% _.each(maint, function(single) {%><%= single.MAINT_ID %>: <%= single.E_DESC %>\n <% });%>\n\n\n Si tienes problemas para completar los mantenimientos dirigete con " + supervisor + "\n Para cerrar revisar y completar tus mantenimientos: http://jupiter.avagotech.net/apps/qa/maintenance/login.php Si tu correo '<%=maint[0].EMAIL%>' no corresponde con la cuenta '<%= name %>' envia un correo a aldo.mendez@avagotech.com para hacer la correccion")({
           maint: el,
           name: name
         });
-        console.log(Message);
         server.send({
           from: 'MaintBot <cyopticsmexico@gmail.com>',
           to: "aldo.mendez@avagotech.com," + email,
           subject: 'Mantenimientos',
           text: Message
-        }, function(err, message) {});
+        }, function(err, message) {
+          if (err) {
+            console.log("Error: " + err);
+          }
+          if (message) {
+            return console.log("Message: " + message);
+          }
+        });
       }
       if (name === 'null') {
         return _.map(_.groupBy(el, 'SUPERVISOR'), function(el, name) {
           email = el[0].SUP_EMAIL;
-          Message = _.template("<%= maint[0].SUPERVISOR %>\n\n Tienes [<%= maint.length%>] mantenimientos pendientes de asignar, asignalos a la brevedad posible:\n\n <% _.each(maint, function(single) {%><%= single.MAINT_ID %>: <%= single.E_DESC %>\n <% });%>\n\n\n Si tu correo '<%=maint[0].SUP_EMAIL%>' no corresponde con la cuenta '<%= name %>' envia un correo a aldo.mendez@avagotech.com para hacer la correccion")({
+          Message = _.template("<%= maint[0].SUPERVISOR %>\n\n Tienes [<%= maint.length%>] mantenimientos pendientes de asignar, asignalos a la brevedad posible:\n\n <% _.each(maint, function(single) {%><%= single.MAINT_ID %>: <%= single.E_DESC %>\n <% });%>\n\n\n Asigna  tus mantenimientos en la siguiente direccion: http://jupiter.avagotech.net/apps/qa/maint/index.php Si tu correo '<%=maint[0].SUP_EMAIL%>' no corresponde con la cuenta '<%= name %>' envia un correo a aldo.mendez@avagotech.com para hacer la correccion")({
             maint: el,
             name: name
           });
-          console.log(Message);
           return server.send({
             from: 'MaintBot <cyopticsmexico@gmail.com>',
             to: "aldo.mendez@avagotech.com," + email,
             subject: 'Mantenimientos',
             text: Message
-          }, function(err, message) {});
+          }, function(err, message) {
+            if (err) {
+              console.log("Error: " + err);
+            }
+            if (message) {
+              return console.log("Message: " + message);
+            }
+          });
         });
       }
     });
+    if (unasigned.length === 0) {
+      unasigned.push('Todos los usuarios estan capturados');
+    }
     return server.send({
       from: 'MaintBot <cyopticsmexico@gmail.com>',
       to: "aldo.mendez@avagotech.com",
       subject: 'Mantenimientos: faltantes para capturar',
       text: "" + (JSON.stringify(unasigned))
-    }, function(err, message) {});
+    }, function(err, message) {
+      if (err) {
+        console.log("Error: " + err);
+      }
+      if (message) {
+        return console.log("Message: " + message);
+      }
+    });
   });
 
 }).call(this);
